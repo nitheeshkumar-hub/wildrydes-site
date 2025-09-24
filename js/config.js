938f183bc6d11944473849fb8e67540a30b03a15
nitheeshkumar-hub <nitheeshkumarmarasami@gmail.com>
2025-09-24 18:13:39 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_dCpYz2Exf', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ap-south-1_dCpYz2Exf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4obnts4ue8l9323gq1l52vmggg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
